dae805df4f073c0a762dfe8c02108fb00eb26ad4
chengmiao <673094766@qq.com>
2019-02-17 20:19:04 +0800

    11

--- src/tcpclient.cpp
+++ src/tcpclient.cpp
@@ -67,7 +67,7 @@ int TcpClient::connect (const char * pHost, uint16_t port, bool reconnect  )
 		int ret = do_connect(); 
 		m_recv_thread = std::thread(std::bind(&TcpClient::run, this)); 
 
-		std::cout << std::itoa(ret) << std::endl;
+		std::cout << ::itoa(ret) << std::endl;
 		return ret; 
 	}
 	return 0; 
