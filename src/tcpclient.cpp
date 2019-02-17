658b9a6904e93f731fbc5502817a29e06e3cb6a2
chengmiao <673094766@qq.com>
2019-02-17 19:55:30 +0800

    11

--- src/tcpclient.cpp
+++ src/tcpclient.cpp
@@ -17,7 +17,7 @@ int TcpClient::do_connect()
 	fd = socket(AF_INET , SOCK_STREAM, 0); 
 	if (-1 == fd)
 	{
-		elog("open socket failed"); 
+		//elog("open socket failed"); 
 		return -1; 
 	}
 
@@ -28,7 +28,7 @@ int TcpClient::do_connect()
 	addr.sin_port = htons(m_port);
 	if (-1 == ::connect(fd, (sockaddr*)&addr, sizeof(addr)))
 	{
-		dlog("failed to connect server error %s:%d",inet_ntoa(addr.sin_addr),m_port);
+		//dlog("failed to connect server error %s:%d",inet_ntoa(addr.sin_addr),m_port);
 		::close(fd); 
 		return -1;
 	}
@@ -39,21 +39,21 @@ int TcpClient::do_connect()
 
 int TcpClient::connect (const char * pHost, uint16_t port, bool reconnect  )
 {
-	dlog("try to connecto to %s ,port %d",pHost, port); 
+	//dlog("try to connecto to %s ,port %d",pHost, port); 
 	m_port =  port; 
 	m_host = pHost; 
 	struct hostent *hp = gethostbyname(m_host.c_str());
 	if(hp != NULL)  {
-		dlog("host name %s : ", hp->h_name);
+		//dlog("host name %s : ", hp->h_name);
 		unsigned int i = 0;
 		while ( hp -> h_addr_list[i] != NULL) {
-			dlog ( "address : %s ", inet_ntoa( *( struct in_addr*)( hp -> h_addr_list[i])));
+			//dlog ( "address : %s ", inet_ntoa( *( struct in_addr*)( hp -> h_addr_list[i])));
 			m_hosts.push_back( ((struct in_addr*) hp->h_addr_list[0])->s_addr); 
 			i++;
 		}
 	}
 	else {
-		elog("gethostbyname() failed");
+		//elog("gethostbyname() failed");
 		return -1; 
 	}
 
@@ -84,7 +84,7 @@ int TcpClient::send(const char * pData, uint32_t len)
 	return -1; 
 }
 int TcpClient::on_recv(const char * pData, uint32_t len) {
-	ilog("received data%s" , pData); 
+	//ilog("received data%s" , pData); 
 	return 0;
 }
 
@@ -106,7 +106,7 @@ int TcpClient::do_recv()
 	recv_buf_pos += recvLen; 
 	uint32_t readPos = 0;
 	int pkgLen = read_packet(recv_buf,recv_buf_pos);
-	dlog("package length:  is %d\n",pkgLen);
+	//dlog("package length:  is %d\n",pkgLen);
 	while (pkgLen >0 )
 	{
 		if (readPos + pkgLen <= recv_buf_pos )
@@ -158,11 +158,11 @@ void TcpClient::run()
 		struct timeval tv;
 		tv.tv_sec = 5;
 		tv.tv_usec = 0;
-		dlog(" fd is %d",fd); 
+		//dlog(" fd is %d",fd); 
 		int ret = select(fd+1, &read_fds, NULL, &except_fds, &tv);
 		if (ret == -1)
 		{
-			elog("select error remove %d  from sets", fd);
+			//elog("select error remove %d  from sets", fd);
 			sleep(1); 
 		} else if (ret)
 		{
@@ -172,7 +172,7 @@ void TcpClient::run()
 			}
 			if (FD_ISSET(fd,&except_fds))
 			{
-				dlog("socket error , close connection"); 
+				//dlog("socket error , close connection"); 
 				FD_CLR(fd,&read_fds); 
 				FD_CLR(fd,&except_fds); 
 				on_disconnect(); 
@@ -180,7 +180,7 @@ void TcpClient::run()
 		}
 		else
 		{
-			dlog("select timeout"); 
+			//dlog("select timeout"); 
 			if (!is_connected)
 			{
 				do_connect(); 
