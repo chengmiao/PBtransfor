6c99a25685d23f1b12ecf64b8410953095e8dfb7
chengmiao <chengmiao565986@gmail.com>
2019-02-17 22:09:52 +0800

    11

--- src/TransPBClient.cpp
+++ src/TransPBClient.cpp
@@ -5,6 +5,8 @@
 
 int TransPBClient::on_recv(const char * pData, uint32_t len)
 {
+    std::cout.write(pData, len);
+    std::cout << std::endl;
     std::cout << "Enter message" << std::endl;
     char request[1024];
     std::cin.getline(request, 1024);
