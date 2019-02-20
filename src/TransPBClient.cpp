0a61ce837f6fca06ca25912085edfa29651d5502
chengmiao <673094766@qq.com>
2019-02-20 14:07:18 +0800

    11

--- src/TransPBClient.cpp
+++ src/TransPBClient.cpp
@@ -5,8 +5,8 @@
 
 int TransPBClient::on_recv(const char * pData, uint32_t len)
 {
-    std::cout.write(pData, len);
-    std::cout << std::endl;
+    //std::cout.write(pData, len);
+    //std::cout << std::endl;
     return 0;
 }
 
