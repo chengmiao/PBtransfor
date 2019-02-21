d48f162446b9757556086d77583a3ccf3fc94f2c
chengmiao <673094766@qq.com>
2019-02-21 15:11:53 +0800

    11

--- unpack.lua
+++ unpack.lua
@@ -1,5 +1,42 @@
-local data = "0080chengmiao@lindadfdfdfdfdfdfddf"
+local data = "\0\0\a0chengmiao@lindadfdfdfdfdfdfddf"
 
-local head = string.sub(data, 1, 3)
-print(tonumber(head))
-return head
+local num1, num2, num3 = string.byte(data, 1, 3)
+
+local len = bufToInt32("\0", num1, num2, num3)
+
+-- 二进制=ascii
+-- 二进制转int
+function bufToInt32(num1, num2, num3, num4)
+    local num = 0;
+    num = num + leftShift(num1, 24);
+    num = num + leftShift(num2, 16);
+    num = num + leftShift(num3, 8);
+    num = num + num4;
+    return num;
+end
+ 
+-- int转二进制
+function int32ToBufStr(num)
+    local str = "";
+    str = str .. numToAscii(rightShift(num, 24));
+    str = str .. numToAscii(rightShift(num, 16));
+    str = str .. numToAscii(rightShift(num, 8));
+    str = str .. numToAscii(num);
+    return str;
+end
+ 
+-- 左移
+function leftShift(num, shift)
+    return math.floor(num * (2 ^ shift));
+end
+ 
+-- 右移
+function rightShift(num, shift)
+    return math.floor(num / (2 ^ shift));
+end
+
+-- 转成Ascii
+function numToAscii(num)
+    num = num % 256;
+    return string.char(num);
+end
