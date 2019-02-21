0698f9434ca5b090ce273e17e77850e243d4b14e
chengmiao <673094766@qq.com>
2019-02-21 14:19:34 +0800

    11

--- unpack.lua
+++ unpack.lua
@@ -1,5 +1,5 @@
 local data = "0080chengmiao@lindadfdfdfdfdfdfddf"
 
-local head1, head2, head3 = string.byte(data, 1, 3)
-print(string.byte(data, 1, 3))
-return head1..head2..head3
+local head = string.sub(data, 1, 3)
+print(tonumber(head)
+return head
